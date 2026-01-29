b623e71eafcaca7ec3164c0f478c0e44f5708e30
Renato Oliveira <renatog.oliveira17@gmail.com>
2026-01-29 02:52:42 -0300

    setup of project

---
+++ src/test/java/com/renato/projects/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.projects.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
